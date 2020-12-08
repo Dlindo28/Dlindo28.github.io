fef6045a1db9cd33f4573060b34a241f87b6c2b8
Dlindo28 <dilindo28@gmail.com>
2020-12-07 16:14:27 -0800

    Updates

---
+++ precache-manifest.35e898dc9c674659920774705924666c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825434ceaba058a3eb25e536cba8f8cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b860abd7364b54298ce5",
+    "url": "/static/css/main.1323799f.chunk.css"
+  },
+  {
+    "revision": "39a3a6315a6f92c030ec",
+    "url": "/static/js/2.05926976.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.05926976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b860abd7364b54298ce5",
+    "url": "/static/js/main.9c596c6e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "07be33850c0003cee902ebe2192b469c",
+    "url": "/static/media/BG Pic.07be3385.png"
+  },
+  {
+    "revision": "fb1e34678b049ac6e41df7fea0acfcf4",
+    "url": "/static/media/acnh-1.fb1e3467.gif"
+  },
+  {
+    "revision": "91c81a0600a73fe3baae3a2df619f59d",
+    "url": "/static/media/headshot.91c81a06.png"
+  },
+  {
+    "revision": "60e0e169db57bec0bbbd4d70b92f39a4",
+    "url": "/static/media/resume.60e0e169.pdf"
+  },
+  {
+    "revision": "7d25ccf2f796122af7eef25eaa92cbdf",
+    "url": "/static/media/resume.7d25ccf2.jpg"
+  }
+]);
