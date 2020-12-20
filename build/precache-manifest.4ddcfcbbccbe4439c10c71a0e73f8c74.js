7e6b8f38389c024b596c30bfa95cd500e57fa98f
Dlindo28 <dilindo28@gmail.com>
2020-12-20 02:15:58 -0800

    added build

---
+++ build/precache-manifest.4ddcfcbbccbe4439c10c71a0e73f8c74.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75275fd8bcf880cca1871a26e2d98fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d06b160ab8c0ac5353",
+    "url": "/static/css/main.53c62b7e.chunk.css"
+  },
+  {
+    "revision": "2ce8c9f7805b2b7f112f",
+    "url": "/static/js/2.da092f9c.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.da092f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d06b160ab8c0ac5353",
+    "url": "/static/js/main.f5f9d0f1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "07be33850c0003cee902ebe2192b469c",
+    "url": "/static/media/BG Pic.07be3385.png"
+  },
+  {
+    "revision": "fb1e34678b049ac6e41df7fea0acfcf4",
+    "url": "/static/media/acnh-1.fb1e3467.gif"
+  },
+  {
+    "revision": "e3c9d8d68a3d271bed52642a6c0b1515",
+    "url": "/static/media/dl-imaging-preview.e3c9d8d6.jpg"
+  },
+  {
+    "revision": "91c81a0600a73fe3baae3a2df619f59d",
+    "url": "/static/media/headshot.91c81a06.png"
+  },
+  {
+    "revision": "60e0e169db57bec0bbbd4d70b92f39a4",
+    "url": "/static/media/resume.60e0e169.pdf"
+  },
+  {
+    "revision": "7d25ccf2f796122af7eef25eaa92cbdf",
+    "url": "/static/media/resume.7d25ccf2.jpg"
+  }
+]);
